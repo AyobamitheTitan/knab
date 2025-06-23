a3938451fead8bc95f1889060645a39b5e169efe
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-23 23:55:25 +0100

    add auth routes

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginUserDTO, SignupUserDTO } from './dto/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post("/signup")
+    async signup(@Body() signupUserDTO: SignupUserDTO){
+        return this.authService.signup(signupUserDTO)
+    }
+
+    @Post("/login")
+    async login(@Body() loginUserDTO: LoginUserDTO) {
+        return this.authService.login(loginUserDTO)
+    }
+}
