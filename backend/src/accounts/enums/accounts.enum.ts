7fff62e7ee6bd8f3909152ea41d671e8366321c2
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-24 20:31:31 +0100

    define account tier enums

---
+++ backend/src/accounts/enums/accounts.enum.ts
@@ -0,0 +1,15 @@
+export enum AccountTypeEnum {
+    SAVINGS = "SAVINGS",
+    CURRENT = "CURRENT"
+}
+
+export enum AccountStatusEnum {
+    APPROVED = "APPROVED",
+    BLOCKED = "BLOCKED"
+}
+
+export enum AccountTierEnum {
+    OBSIDIAN = "OBSIDIAN", // or JJC/ Jadeite
+    EMERALD = "EMERALD", // or Big Boy/ Beryl
+    ALEXANDRITE = "ALEXANDRITE" // or Agba
+}
