2692fd9fdd2ea59da8222f6203e684f92bdc22ad
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-24 20:33:55 +0100

    define new transaction enums

---
+++ backend/src/transactions/enums/transactions.enum.ts
@@ -0,0 +1,11 @@
+export enum TransactionTypeEnum {
+    WITHDRAW="WITHDRAW",
+    DEPOSIT="DEPOSIT"
+}
+
+export enum TransactionStatusEnum{
+    SUCCESSFUL="SUCCESSFUL",
+    FAILED="FAILED",
+    REVERSED="REVERSED",
+    PENDING="PENDING"
+}
