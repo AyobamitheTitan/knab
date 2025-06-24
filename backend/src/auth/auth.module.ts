b64aa805880629b063b5316ebdb26e9e5f4c3e2f
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-24 20:32:17 +0100

    include new providers and use .env secrets

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TokenService } from 'src/token/token.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [
@@ -12,13 +14,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: configService.get<string>("JWT_EXPIRESIN") },
       }),
       inject: [ConfigService],
     }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, TokenService, UserService],
 })
 export class AuthModule { }
