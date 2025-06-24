5e635680bd780039b619c4acdf027db5a7073774
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-24 20:30:15 +0100

    include transactions and accounts modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { AuthMiddleware } from './auth/auth.middleware';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL ?? ''),
     AuthModule,
+    AccountsModule,
+    TransactionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes("accounts","transactions")
+  }
+}
+
