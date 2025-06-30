ea9fd222e95a90e8c8fce4e866b85205e557d611
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:24:34 +0100

    update module to include new providers

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,26 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
+import { User, UserSchema } from '../user/schemas/user.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TokenService } from 'src/token/token.service';
 import { UserService } from 'src/user/user.service';
+import { AuthResolver } from './auth.resolver';
+import { AccountsService } from 'src/accounts/accounts.service';
+import { Account, AccountSchema } from 'src/accounts/schemas/accounts.schema';
+import { TransactionsService } from 'src/transactions/transactions.service';
+import { Transactions, TransactionsSchema } from 'src/transactions/schemas/transactions.schema';
 
 @Module({
   imports: [
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>("JWT_EXPIRESIN") },
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRESIN') },
       }),
       inject: [ConfigService],
     }),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }, {name: Account.name, schema: AccountSchema}, {name: Transactions.name, schema: TransactionsSchema}])
   ],
-  controllers: [AuthController],
-  providers: [AuthService, TokenService, UserService],
+  providers: [AuthService, TokenService, UserService, AuthResolver, AccountsService, TransactionsService],
 })
-export class AuthModule { }
+export class AuthModule {}
