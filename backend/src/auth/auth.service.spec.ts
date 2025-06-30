fc5db8061be0e836206c6a12625fa389c139faea
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:24:47 +0100

    new tests

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -2,53 +2,56 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { getModelToken } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
+import { User } from '../user/schemas/user.schema';
 import { Model } from 'mongoose';
-import { LoginUserDTO, SignupUserDTO } from './dto/user.dto';
+import { SignupUserDTO } from './dto/auth.dto';
 
 const userModelMock = {
   signup: jest.fn(),
-  login: jest.fn()
-}
+  login: jest.fn(),
+};
 
 describe('AuthService', () => {
   let service: AuthService;
-  let model: jest.Mocked<Model<User>>
+  let model: jest.Mocked<Model<User>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [{
-        provide: getModelToken(User.name),
-        useValue: Model
-      }, AuthService, JwtService],
+      providers: [
+        {
+          provide: getModelToken(User.name),
+          useValue: Model,
+        },
+        AuthService,
+        JwtService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
-    model = module.get(getModelToken("User"))
+    model = module.get(getModelToken('User'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  describe("signup", () => {
-    it("should signup a new user", async () => {
+  describe('signup', () => {
+    it('should signup a new user', async () => {
       const mockedUser: SignupUserDTO = {
-        dob: new Date("2005-09-08"),
-        password: "password",
-        email: "mockuser@gmail.com",
+        dob: new Date('2005-09-08'),
+        password: 'password',
+        email: 'mockuser@gmail.com',
         phone_number: 234809123451884,
-        last_name: "Onabanjo",
-        first_name: "Oluwademilade"
-      }
-      
-      const result = service.signup(mockedUser)
+        last_name: 'Onabanjo',
+        first_name: 'Oluwademilade',
+      };
+
+      const result = service.signup(mockedUser);
 
       expect(result).toEqual({
-        email: "mockuser@gmail.com",
+        email: 'mockuser@gmail.com',
         phone_number: 234809123451884,
-      })
-    })
-  })
-
+      });
+    });
+  });
 });
