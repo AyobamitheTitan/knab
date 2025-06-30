9087fa3ac97582cf98647de1fb13a6fa96e4753b
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:27:50 +0100

    update transactions service to work with graphql

--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -1,52 +1,53 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { TokenService } from 'src/token/token.service';
-import { UserService } from 'src/user/user.service';
-import { SetTransactionPinDTO } from './dto/transactions.dto';
-import { TransactionStatusEnum, TransactionTypeEnum } from './enums/transactions.enum';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  TransactionStatusEnum,
+  TransactionTypeEnum,
+} from './enums/transactions.enum';
 import { InjectModel } from '@nestjs/mongoose';
 import { Transactions } from './schemas/transactions.schema';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class TransactionsService {
-    constructor(
-        @InjectModel(Transactions.name) private transactionModel: Model<Transactions>,
-        private tokenService: TokenService, 
-        private userService: UserService
-    ) { }
+  constructor(
+    @InjectModel(Transactions.name)
+    private transactionModel: Model<Transactions>,
+  ) {}
 
-    async setTransactionPin(authHeader: String, payload: SetTransactionPinDTO) {
-        if (payload.pin !== payload.confirmPin) {
-            throw new BadRequestException("Pins do not match")
-        }
+  async create(
+    type: TransactionTypeEnum,
+    amount: number,
+    status: TransactionStatusEnum,
+    description: string | undefined,
+    account_id: string,
+  ) {
+    return await this.transactionModel.create({
+      type,
+      amount,
+      status,
+      description,
+      account_id,
+      date: new Date()
+    });
+  }
 
-        const user_id = (await this.tokenService.decode(authHeader)).sub
-        
-        await this.userService.setTransactionPin(user_id, payload.pin)
-        return { data: true }
+  async findById(id: string) {
+    const transaction = await this.transactionModel.findOne({ _id: id }).exec();
+    if (!transaction) {
+      throw new NotFoundException('Transaction not found');
     }
+    return transaction;
+  }
 
-    async create(
-        type: TransactionTypeEnum,
-        amount: number,
-        status: TransactionStatusEnum,
-        description: string,
-        account_id: string
-    ){
-        return await this.transactionModel.create({type, amount, status,description,account_id})
-    }
+  async findSome(account_id: string, limit: number, skip: number) {
+    return await this.transactionModel
+      .find({ account_id })
+      .limit(limit)
+      .skip(skip)
+      .exec();
+  }
 
-
-    async findById(id: String){
-        const transaction = await this.transactionModel.findOne({_id:id}).exec()
-        if (!transaction) {
-            throw new NotFoundException("Transaction not found")
-        }
-        return transaction
-    }
-
-
-    async findSome(account_id: String, limit = 10){
-        return await this.transactionModel.find({account_id}).limit(limit).exec()
-    }
+  async getLatest(account_id: string) {
+    return await this.transactionModel.findOne({ account_id }).exec();
+  }
 }
