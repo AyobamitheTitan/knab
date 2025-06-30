4627a2425e8527a29f6cd1bd744deb7d438dc089
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:31:17 +0100

    define main pages layout

---
+++ frontend/src/routes/(main)/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Sidebar from "../../components/Sidebar.svelte";
+
+    import Dashboard from '../../components/Dashboard.svelte';
+    import { page } from "$app/stores";
+
+    const {data} = $props()
+    
+    const account = data.account
+</script>
+
+<Sidebar >
+
+{#if $page.url.pathname !== "/settings"}
+<Dashboard {account}/>
+    
+{/if}
+
+<slot></slot>
+
+</Sidebar>
