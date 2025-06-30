3dcd7ec458764d043a3d5516776efffa6a6fbc57
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:29:59 +0100

    update layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css";
+    import { page } from "$app/stores";
+
+    const nonAuthenticatedRoutes = ["/login", "/signup"];
+    console.log($page.url.pathname)
+</script>
+
+{#if nonAuthenticatedRoutes.includes($page.url.pathname) || $page.url.pathname === "/"}
+    <nav class="flex justify-center text-3xl p-2 bg-slate-50">
+        <h1>KNAB&trade;</h1>
+    </nav>
+{/if}
+<slot></slot>
