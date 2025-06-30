12124003406d26e4a63805d5544eb1cb307687e4
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:31:51 +0100

    add settings page

---
+++ frontend/src/routes/(main)/settings/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import SetPinPopup from "../../../components/SetPinPopup.svelte";
+    const {data} = $props()    
+</script>
+
+<div class="px-20 py-20 flex flex-col justify-center">
+    <SetPinPopup account_id={data.account._id}/>
+    <hr />
+</div>
