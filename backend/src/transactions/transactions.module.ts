a1c2469fd9067cadb77ed620471a1718e4dd5631
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:27:21 +0100

    update transaction module for graphql

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,28 +1,28 @@
 import { Module } from '@nestjs/common';
-import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TokenService } from 'src/token/token.service';
-import { UserService } from 'src/user/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/auth/schemas/user.schema';
-import { Transactions, TransactionsSchema } from './schemas/transactions.schema';
+import {
+  Transactions,
+  TransactionsSchema,
+} from './schemas/transactions.schema';
+import { TransactionsResolver } from './transactions.resolver';
 
 @Module({
   imports: [
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>("JWT_EXPIRESIN") },
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRESIN') },
       }),
       inject: [ConfigService],
     }),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }, {name: Transactions.name, schema: TransactionsSchema}])
-
+    MongooseModule.forFeature([
+      { name: Transactions.name, schema: TransactionsSchema },
+    ]),
   ],
-  providers: [TransactionsService, TokenService, UserService],
-  controllers: [TransactionsController]
+  providers: [TransactionsService, TransactionsResolver],
 })
-export class TransactionsModule { }
+export class TransactionsModule {}
