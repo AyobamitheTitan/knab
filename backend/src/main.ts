b91e53b176bab8c8c72a1e9021a7e67dc610c561
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 23:53:37 +0100

    update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/http-exception/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST',],
+    credentials: true
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix("api")
