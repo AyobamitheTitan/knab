68ae4fa74d3aedb35e7902a4d31ab1af9d43faa2
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:23:44 +0100

    define necessary enums

--- backend/src/accounts/enums/accounts.enum.ts
+++ backend/src/accounts/enums/accounts.enum.ts
@@ -1,15 +1,15 @@
 export enum AccountTypeEnum {
-    SAVINGS = "SAVINGS",
-    CURRENT = "CURRENT"
+  SAVINGS = 'SAVINGS',
+  CURRENT = 'CURRENT',
 }
 
 export enum AccountStatusEnum {
-    APPROVED = "APPROVED",
-    BLOCKED = "BLOCKED"
+  APPROVED = 'APPROVED',
+  BLOCKED = 'BLOCKED',
 }
 
 export enum AccountTierEnum {
-    OBSIDIAN = "OBSIDIAN", // or JJC/ Jadeite
-    EMERALD = "EMERALD", // or Big Boy/ Beryl
-    ALEXANDRITE = "ALEXANDRITE" // or Agba
+  OBSIDIAN = 'OBSIDIAN', // or JJC/ Jadeite
+  EMERALD = 'EMERALD', // or Big Boy/ Beryl
+  ALEXANDRITE = 'ALEXANDRITE', // or Agba
 }
