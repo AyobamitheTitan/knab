9cf8e72b5898218a48a7ca131ce7eb897e56cdb2
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:22:11 +0100

    include new providers and others

--- backend/src/accounts/accounts.module.ts
+++ backend/src/accounts/accounts.module.ts
@@ -1,29 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
-import { AccountsController } from './accounts.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Account, AccountSchema } from './schemas/accounts.schema';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TokenService } from 'src/token/token.service';
 import { TransactionsService } from 'src/transactions/transactions.service';
-import { Transactions, TransactionsSchema } from 'src/transactions/schemas/transactions.schema';
-import { UserService } from 'src/user/user.service';
-import { User, UserSchema } from 'src/auth/schemas/user.schema';
+import {
+  Transactions,
+  TransactionsSchema,
+} from 'src/transactions/schemas/transactions.schema';
+import { AccountsResolver } from './accounts.resolver';
 
 @Module({
   imports: [
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>("JWT_EXPIRESIN") },
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRESIN') },
       }),
       inject: [ConfigService],
     }),
-    MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }, {name: Transactions.name, schema: TransactionsSchema}, {name: User.name, schema:UserSchema}]), 
+    MongooseModule.forFeature([
+      { name: Account.name, schema: AccountSchema },
+      { name: Transactions.name, schema: TransactionsSchema },
+    ]),
   ],
-  providers: [AccountsService, TokenService, TransactionsService, UserService],
-  controllers: [AccountsController]
+  providers: [
+    AccountsService,
+    TokenService,
+    TransactionsService,
+    AccountsResolver,
+  ],
 })
 export class AccountsModule {}
