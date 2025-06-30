c3fc8447aaa584e16d013138caef036b3dcb2cb2
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:26:33 +0100

    update token service

--- backend/src/token/token.service.ts
+++ backend/src/token/token.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import Payload from './interfaces/payload.interface';
 
 @Injectable()
 export class TokenService {
-    constructor(private jwtService: JwtService){}
+  constructor(private jwtService: JwtService) { }
 
-    async generate(payload: Payload){
-        return await this.jwtService.signAsync(payload)
-    }
+  async generate(payload: Payload) {
+    return await this.jwtService.signAsync(payload);
+  }
 
-    async decode(authHeader: String): Promise<Payload>{
-        const token = authHeader.split(" ")[1]
-        return this.jwtService.decode(token)
+  async decode(authHeader: string): Promise<Payload> {
+    try {
+      const token = authHeader.split(' ')[1];      
+      return this.jwtService.decode(token);
+    } catch (err) {
+      throw new UnauthorizedException("Invalid token")
     }
+  }
 }
