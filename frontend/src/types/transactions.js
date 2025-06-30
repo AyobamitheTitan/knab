a8025490d8836212d0366e2ae345e7ee55f1384c
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-06-30 21:32:36 +0100

    define transaction types

---
+++ frontend/src/types/transactions.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} TransactionHistory
+ * @property {number}  amount
+ * @property {string}  account_id
+ * @property {string} type 
+ * @property {string} status 
+ * @property {Date} date
+ * @property {string} description
+ * @property {string} id
+ */
