f05953cbeefb64d02964779036b7d4d6e29f08e9
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-09-05 22:39:11 +0100

    update app ports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix("api")
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(10000, '0.0.0.0');
 }
 bootstrap();
